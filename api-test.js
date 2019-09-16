cacb27c1024f3f1958c2e9bc99d9cd85dcf5dd6c
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-09-16 23:19:52 +0200

    Setup environment variables and serverless config and local data for lineups

--- api-test.js
+++ api-test.js
@@ -4,8 +4,10 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
     try {
         const result = await getTeam(33);
+        console.log("API KEY", process.env.API_KEY);
         return success(result);
     } catch (e) {
+        console.log("API KEY", process.env.API_KEY);
         return failure({ status: false, message: e });
     }
 }
--- get-team.js
+++ get-team.js
@@ -1,17 +1,21 @@
 import {call} from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+import lineups from "./data/lineups.js";
+import findKey from "lodash/findKey";
 
 export async function main(event, context) {
+  const teamId = event.pathParameters.id;
   const params = {
     TableName: "Teams",
     Key: {
-      teamId: event.pathParameters.id
+      teamId
     }
   };
   try {
     const result = await call("get", params);
+    const lineup = findKey(lineups, (item, index) => index === teamId);
     if (result.Item) {
-      return success(result.Item);
+      return success({...result.Item, ...lineup});
     } else {
       return failure({ status: false, error: "Team not found"});
     }
