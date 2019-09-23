620ad6afccf3b71c7ba8f57a20d04cc7930fe10d
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-09-23 21:40:59 +0200

    Add static lineup data for liverpool.

--- data/lineups.js
+++ data/lineups.js
@@ -1,5 +1,7 @@
 import manchesterUnited from "./manchester_united";
+import liverpool from "./liverpool";
 
 export default {
-    1: manchesterUnited
+    1: manchesterUnited,
+    2: liverpool
 };
---
+++ data/liverpool.js
@@ -0,0 +1,55 @@
+export default {
+    goal_keeper: [
+        {
+            player: 280,
+            number: 13
+        }
+    ],
+    defenders: [
+        {
+            player: 289,
+            number: 26
+        },
+        {
+            player: 290,
+            number: 4
+        },
+        {
+            player: 284,
+            number: 12
+        },
+        {
+            player: 283,
+            number: 66
+        }
+    ],
+    midfield_defense: [],
+    midfield_offense: [
+        {
+            player: 300,
+            number: 5
+        },
+        {
+            player: 299,
+            number: 3
+        },
+        {
+            player: 292,
+            number: 14
+        }
+    ],
+    striker: [
+        {
+            player: 304,
+            number: 10
+        },
+        {
+            player: 302,
+            number: 9
+        },
+        {
+            player: 306,
+            number: 11
+        },
+    ]
+};
