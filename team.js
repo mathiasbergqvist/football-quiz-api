2a2b726b667193a446be03eb3ed2824c1fd82344
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-09-23 21:29:18 +0200

    Lambda function for getting team info based on id done

--- team.js
+++ team.js
@@ -2,6 +2,7 @@ import {call} from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 import lineups from "./data/lineups.js";
 import findKey from "lodash/findKey";
+import { getSquad } from "./libs/api";
 
 export async function main(event, context) {
   const teamId = event.pathParameters.id;
@@ -12,14 +13,19 @@ export async function main(event, context) {
     }
   };
   try {
-    const result = await call("get", params);
-    const lineup = findKey(lineups, (item, index) => index === teamId);
-    if (result.Item) {
-      return success({...result.Item, ...lineup});
+    const teamInfo = await call("get", params);
+    const squad = await getSquad(teamInfo.Item.apiId, "2018-2019");
+    const key = findKey(lineups, (item, index) => index === teamId);
+    if (teamInfo.Item) {
+      return success({
+        team: teamInfo.Item,
+        lineup: lineups[key],
+        squad
+      });
     } else {
       return failure({ status: false, error: "Team not found"});
     }
   } catch (e) {
-    return failure({status: false});
+    return failure({status: false, message: e});
   }
 };
