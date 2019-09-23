7d64fca012465691c28513ecaa696ba4d70710df
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-09-23 21:27:27 +0200

    Refactor static data

---
+++ data/manchester_united.js
@@ -0,0 +1,56 @@
+export default {
+    goal_keeper: [
+        {
+            player: 882,
+            number: 1
+        }
+    ],
+    defenders: [
+        {
+            player: 891,
+            number: 23
+        },
+        {
+            player: 2935,
+            number: 5
+        },
+        {
+            player: 889,
+            number: 2
+        },
+        {
+            player: 18846,
+            number: 29
+        }
+    ],
+    midfield_defense: [
+        {
+            player: 904,
+            number: 6
+        },
+        {
+            player: 903,
+            number: 9
+        }
+    ],
+    midfield_offense: [
+        {
+            player: 19329,
+            number: 23
+        },
+        {
+            player: 900,
+            number: 14
+        },
+        {
+            player: 908,
+            number: 9
+        }
+    ],
+    striker: [
+        {
+            player: 909,
+            number: 10
+        }
+    ]
+};
